af449e0b506a696c4f63032408795726e11a4157
Robin van Sloten <Neograph734@users.noreply.github.com>
2017-08-18 21:48:35 +0200

    Update sass-yaml.rb
    Changed the gem to read an entire YAML file into a SASS map.

--- lib/sass-yaml.rb
+++ lib/sass-yaml.rb
@@ -1,28 +1,39 @@
 require 'yaml'
 
 module Sass::Script::Functions
-  def yaml(variable)
-  	config_file = 'config/config.yml'
-
-  	if (File.file?(config_file))
-	  	obj = YAML.load_file(config_file)
-      if !obj['variables'][variable.to_s].nil?
-        value = obj['variables'][variable.to_s]
-        if(value.is_a?(String))
-          return Sass::Script::Value::String.new(value.to_s)
-        elsif (value.is_a?(Array))
-          value.each_with_index do |elem, i|
-            value[i]= Sass::Script::Value::String.new(elem.to_s)
-          end
-          return Sass::Script::Value::List.new(value, ",")
-        end
-      else
-        return Sass::Script::Value::String.new('not-exists')
+# @param [Hash]  value
+# @return [Sass::Script::Value::Map or Sass::Script::Value::List] 
+  def convert_to_base(value) # part of this function is copied from https://github.com/fabiofabbrucci/sass-yaml/blob/master/lib/sass-yaml.rb
+    if value.is_a?(String) 
+      return Sass::Script::Value::String.new(value.to_s)
+    elsif value.is_a?(Fixnum) 
+      return Sass::Script::Value::Number.new(value)
+    elsif value.is_a?(Array)
+      value.each_with_index do |elem, i|
+        value[i]= Sass::Script::Value::String.new(elem.to_s)
       end
-  	else
-  		p 'Config File not exists. We are looking for a config/config.yml'
-  	end
+      return Sass::Script::Value::List.new(value)
+    elsif value.is_a?(Hash)
+      result = Hash.new
+      value.each {|the_key, the_value|
+        result[convert_to_base(the_key)]=convert_to_base(the_value)
+      }
+      return Sass::Script::Value::Map.new(result)
+    end
+    p 'Error parsing ' + value.class
+  end
 
-    return Sass::Script::Value::String.new('auto')
+  def yaml_load(file_name)
+    # Required because the filename is escaped twice: "\"path/to/file\""
+    file_name = file_name.to_s[1..-2]
+    if (File.file?(file_name))
+      file_content = YAML.load_file(file_name)
+      if (file_content.is_a?(Hash))
+        return convert_to_base(file_content)
+      end
+      p 'Parse YAML content.'
+    else
+      p 'The requested file could not be found.'
+    end
   end
 end
